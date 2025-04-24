5da130c49d7030723b0a372525b3aea7ce907cf8
PRPRSPR <s.subin.0208@gmail.com>
2025-04-24 18:19:25 +0900

    250424-react 맛보기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <div>
+    <div>이 녀석은 헤더입니다</div>
+    {/* 모든 작업은 한 태그 안에서만 */}
+    {/* App 처럼 ()로 묶어도 됨 */}
+  </div>
+}
+
+function Footer(){
+  return (
+    <div>
+      <div style={{color:"red",fontSize:"20px"}}>이 녀석은 푸터입니다</div>
+      {/* style은 중괄호 2개 {{}} color : >>> " red " <<<   font-size =>> fontSize */}
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      {/* 위에서 만든 헤더가 부착됨 */}
+      <div>Hello React</div>
+      <Footer></Footer>
     </div>
   );
 }
